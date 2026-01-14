dd3d4ac98401ab571112b4c67680f4e3f7f5afa5
artitaya <artitaya2466@gmail.com>
2026-01-14 14:12:41 +0700

    unittest register

--- auth-service/internal/application/usecase/register_test.go
+++ auth-service/internal/application/usecase/register_test.go
@@ -1,40 +1,71 @@
 package usecase
 
-// import (
-// 	"context"
-// 	"realtime_web_socket_game_server/auth-service/internal/domain"
-// 	"testing"
+import (
+	"realtime_web_socket_game_server/auth-service/internal/domain"
+	"testing"
 
-// 	"github.com/stretchr/testify/mock"
-// )
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
 
-// type MockUserRepo struct {
-// 	mock.Mock
-// }
+type MockUserRepository struct {
+	mock.Mock
+}
 
-// func (m *MockUserRepo) GetByUsername(ctx context.Context, username string) (*domain.User, error) {
-// 	if user, ok := m.Users[username]; ok {
-// 		return user, nil
-// 	}
-// 	return nil, nil
-// }
+func (m *MockUserRepository) GetByUsername(username string) (*domain.User, error) {
+	args := m.Called(username)
+	user, _ := args.Get(0).(*domain.User)
+	return user, args.Error(1)
+}
 
-// func (m *MockUserRepo) CreateUser(user *domain.User) error {
-// 	args := m.Called(user)
-// 	return args.Error(0)
-// }
+func (m *MockUserRepository) CreateUser(user *domain.User) error {
+	args := m.Called(user)
+	return args.Error(0)
+}
 
-// func (m *MockUserRepo) FindEmail(email string) (*domain.User, error) {
-// 	args := m.Called(email)
-// 	if args.Get(0) != nil {
-// 		return args.Get(0).(*domain.User), args.Error(1)
-// 	}
-// 	return nil, args.Error(1)
-// }
+func (m *MockUserRepository) FindEmail(email string) (*domain.User, error) {
+	args := m.Called(email)
+	if args.Get(0) != nil {
+		return args.Get(0).(*domain.User), args.Error(1)
+	}
+	return nil, args.Error(1)
+}
 
-// func TestRegisterUsecase(t *testing.T) {
-// 	mockRepo := new(MockUserRepo)
-// 	uc := NewRegisterUsecase(mockRepo)
+func TestRegister_Success(t *testing.T) {
+	// Arrange
+	mockRepo := new(MockUserRepository)
+	uc := NewRegisterUsecase(mockRepo)
 
-// 	_, _ = mockRepo, uc
-// }
+	mockRepo.
+		On("GetByUsername", "alice").
+		Return(nil, nil).
+		Once()
+
+	mockRepo.
+		On(
+			"CreateUser",
+			mock.AnythingOfType("*domain.User"),
+		).
+		Return(nil).
+		Once()
+
+	// Act
+	err := uc.Register("alice", "password123")
+
+	// Assert
+	assert.NoError(t, err)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestRegister_UserAlreadyExists(t *testing.T) {
+	mockRepo := new(MockUserRepository)
+	uc := NewRegisterUsecase(mockRepo)
+
+	mockRepo.On("GetByUsername", "alice").
+		Return(&domain.User{Username: "alice"}, nil).Once()
+
+	err := uc.Register("alice", "password123")
+
+	assert.Error(t, err)
+	mockRepo.AssertNotCalled(t, "CreateUser")
+}
